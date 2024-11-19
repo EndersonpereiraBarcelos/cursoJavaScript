e75e465882e482387352a2ba14876d8e49e7783d
endersonpb0 <endersonpb0@gmail.com>
2024-11-19 17:23:13 -0300

    progresso 26/644

---
+++ OperadoresAritmeticos.js
@@ -0,0 +1,31 @@
+/*
+*Aritméticos
+* + adição, - m Subtração
+* * Divisão, ** Potenciação
+* % Resto da divisão
+
+Precedencia de calculo
+
+()
+**
+/ %
++-
+
+++ Incremento
+-- Decremento
+
+*/
+
+const num1 = 10
+const num2 = 3
+const num3 = 9
+
+console.log("Resto da Divisão",num1 % num2 % num3)
+console.log( "Adição",num1 + num2 + num3)
+console.log( "Subtração",num1 - num2 - num3)
+console.log("Potenciação", num1 ** num2 )
+
+
+let contator = 10
+contator++
+console.log(contator++)
--- TiposPrimitivos.js
+++ TiposPrimitivos.js
@@ -1,3 +1,9 @@
 const name = 'Fulano'
-const name1 = 'Fulano1'
+const name1 = "Fulano1"
 
+const name3 = null
+
+const num1 = 10
+const num2 = 20
+
+console.log(typeof name, name1, name3, num1,num2)
